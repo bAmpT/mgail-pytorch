0066bdd74e50ef47b47577624626e91956e8d2d9
bAmpT <wtamino@hotmail.de>
2022-04-23 22:28:47 +0200

    Add policy modules

--- mgail/network/policy.py
+++ mgail/network/policy.py
@@ -37,3 +37,47 @@ class Policy(nn.Module):
         x = self.fc_out(x)
         return x
     
+
+class StateIndependentPolicy(nn.Module):
+
+    def __init__(self, state_shape, action_shape, hidden_units=(64, 64),
+                 hidden_activation=nn.Tanh()):
+        super().__init__()
+
+        self.net = common.build_mlp(
+            input_dim=state_shape[0],
+            output_dim=action_shape[0],
+            hidden_units=hidden_units,
+            hidden_activation=hidden_activation
+        )
+        self.log_stds = nn.Parameter(torch.zeros(1, action_shape[0]))
+
+    def forward(self, states):
+        return torch.tanh(self.net(states))
+
+    def sample(self, states):
+        return common.reparameterize(self.net(states), self.log_stds)
+
+    def evaluate_log_pi(self, states, actions):
+        return common.evaluate_lop_pi(self.net(states), self.log_stds, actions)
+
+
+class StateDependentPolicy(nn.Module):
+
+    def __init__(self, state_shape, action_shape, hidden_units=(256, 256),
+                 hidden_activation=nn.ReLU(inplace=True)):
+        super().__init__()
+
+        self.net = common.build_mlp(
+            input_dim=state_shape[0],
+            output_dim=2 * action_shape[0],
+            hidden_units=hidden_units,
+            hidden_activation=hidden_activation
+        )
+
+    def forward(self, states):
+        return torch.tanh(self.net(states).chunk(2, dim=-1)[0])
+
+    def sample(self, states):
+        means, log_stds = self.net(states).chunk(2, dim=-1)
+        return common.reparameterize(means, log_stds.clamp_(-20, 2))
